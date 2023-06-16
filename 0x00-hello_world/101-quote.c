1ce4420b7ef8b98c11e2a31b4acc779e7ff6b85e
JHAdd7 <jahrule7@gmail.com>
2023-06-16 00:19:39 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - prints "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+ * main - prints text
  * followed by a new line.
  * Return: Always 1.
  */
 int main(void)
-{write(2, "and that piece of art is usefull\" - Dora Korpar, 2015-10-19\n", 59);
+{write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
