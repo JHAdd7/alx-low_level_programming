966cf3b0aa08a2ba952bf44c44378a0af0880c91
JHAdd7 <jahrule7@gmail.com>
2023-06-18 16:35:29 -0700

    9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Prints all possible combinations of single numbers
+ * Return: Alwasy 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
